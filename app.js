3c5eb07c25f942090a64ea8f0ec12f0c69dd025e
thiago <thiago.w3c@gmail.com>
2024-02-27 11:50:55 -0300

    30. Sending responses

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const server = http.createServer((req, res) => {
   console.log("URL:", req.url);
   console.log("METHOD:", req.method);
   console.log("HEADERS:", req.headers);
-  res.end("Hello World!");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html><body><h1>Hello World</h1></body></html>");
+  res.end("");
 });
 server.listen(3000);
