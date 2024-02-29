1238cc2f18c1a1c5c4eab58f63d50e61813e09a8
thiago <thiago.w3c@gmail.com>
2024-02-28 23:02:25 -0300

    33. Redirecting Requests

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("URL:", req.url);
-  console.log("METHOD:", req.method);
-  console.log("HEADERS:", req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "message");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
-  res.write("<html><body><h1>Hello World</h1></body></html>");
-  res.end("");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen(3000);
