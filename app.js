6991328aef65cd104542577973d3e31eea5c5bc9
thiago <thiago.w3c@gmail.com>
2024-03-07 19:37:53 -0300

    68 - adding 404 page error

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -9,6 +11,10 @@ app.use(bodyParser.urlencoded());
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
