e535278ed22d646b2f314a387ea8e8fbd191b0b6
thiago <thiago.w3c@gmail.com>
2024-03-07 18:59:58 -0300

    61. how middleware works

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
-server.listen(3000);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // pass the request to the next handler
+});
+
+app.use((req, res, next) => {
+  console.log("In the middleware 2");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
