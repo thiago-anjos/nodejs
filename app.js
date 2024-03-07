9fdc2811634542eb9c70edae21c3c01393dc9083
thiago <thiago.w3c@gmail.com>
2024-03-07 19:17:51 -0300

    64. parsing incoming request

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/product", (req, res, next) => {
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
   console.log("In the middleware 2");
-  res.send("Product!");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
