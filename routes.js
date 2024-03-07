9d784864dd541a440edccb3202c038dcd75c39e3
thiago <thiago.w3c@gmail.com>
2024-03-07 17:13:20 -0300

    52-55 restarting debugger automatically with nodemon globally

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ function requestHandler(req, res) {
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      console.log(message);
+      console.log("mensagem", message);
       fs.writeFile("message.txt", message, (err) => {
         if (err) {
           console.error("Error:", err);
@@ -37,7 +37,7 @@ function requestHandler(req, res) {
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
+  res.write("<head><title>My First Page 2</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
