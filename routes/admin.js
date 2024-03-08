c90da0314ce875cbe07bde4e4c3091b1ff84ab79
thiago <thiago.w3c@gmail.com>
2024-03-07 21:33:49 -0300

    73-using helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const rootDir = require("../utils/path");
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  console.log("rootDir", rootDir);
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const rootDir = require("../utils/path");
 
 router.get("/", (req, res, next) => {
-  res.sendfile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendfile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
