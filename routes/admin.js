fd939912040b36b9565433bd426d0864aadbb0cc
thiago <thiago.w3c@gmail.com>
2024-03-10 23:18:49 -0300

    78-80 module introduction

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const path = require("path");
 const rootDir = require("../utils/path");
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   console.log("rootDir", rootDir);
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log("req.body", req.body);
+  products.push({
+    title: req.body.title,
+  });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
+  console.log(adminData.products);
   res.sendfile(path.join(rootDir, "views", "shop.html"));
 });
 
