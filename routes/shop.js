e43203d314f5e996d16e695d6629e2e0ca11dfc6
thiago <thiago.w3c@gmail.com>
2024-03-11 22:49:37 -0300

    81-installing and implemeting pug

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,7 @@ const rootDir = require("../utils/path");
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  console.log(adminData.products);
-  res.sendfile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
